0da46f0ba28f5a416a9f2e6e789923936ec93604
euni00 <euneee2000@naver.com>
2022-08-04 15:30:16 +0900

    study 2022.8.4

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ const player = {
     name: "yeeunlee",
     points: 10,
     fat: true,
-};
+}; 
 
 console.log(player);
 player.fat = false;
@@ -74,10 +74,170 @@ player.points = player.points + 15;
 console.log(player);
 
 console.log(player.name);
-console.log(player["name"]);
-// player.name
+console.log(player["name"]); 
+// player.name 
 
-// Functions part One
-function sayHello() {
-
+// 2.7 Functions part One
+function sayHello(nameOfPerson, age) {
+    console.log("Hello my name is " + nameOfPerson + "and I'm " + age);
 }
+
+
+console.log("hello");
+sayHello("yeeunlee",23);
+
+// 2.8 Functions part Two
+function plus(firstNumber, secondNumber) {
+    console.log(firstNumber + secondNumber);
+}
+// console.log(firstNumber); -> Uncaught ReferenceError: firstNumber is not defined
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+// plus(); NaN -> Not a Number
+plus(8, 60);
+divide(98, 20);
+
+const player2 = {
+    name: "yeeunlee",
+    sayHello: function (otherPersonName) {
+        console.log("Hello!" + otherPersonName + " nice to meet you!");
+    },
+}; 
+
+console.log(player.name);
+player2.sayHello('yeeunlee');
+
+// 2.9 Recap 
+const a2 = 5;
+let isNicoFat = true;
+
+isNicoFat = false;
+console.log(a);
+
+const a3 = true;  // boolean
+const b2 = null; // empty
+let hello;
+console.log(hello);  //undefined 
+const me = "sexy";
+const days = [1, 2, false, true, null, undefined, "text", me];
+
+const toBuy2 = ["potato", "tomato", "pizza"];
+console.log(toBuy2[2]);
+toBuy2[2] = "water";
+console.log(toBuy2[2]);
+toBuy2.push("meat");
+console.log(toBuy2);
+
+// 2.10 Recap 2
+const days2 = ["mon", "tue", "wed"];
+const player3 = {
+    name: "yeeunlee",
+    age: 23,
+};
+
+// console.log(player3, console);
+console.log(player3);
+player3.name = "Amy";
+console.log(player3);
+player3.sexy = "soon";
+console.log(player3);
+
+function plus(potato, salad) {
+    console.log(potato + salad);
+}
+
+function minusFive(potato) {
+    console.log(potato - 5);
+}
+
+// alert("lalalalaa");
+minusFive(10);
+plus(5, 10);
+plus(1.234, 9898);
+plus(9898, 1.234);
+
+const calculator = {
+    add: function (a, b) {
+        // console.log(a + b);
+        console.log("hello");
+        return a + b;  // finish function 
+        console.log("bye bye"); // execute X
+    },
+    minus: function (a, b) {
+        // console.log(a - b);
+        return  a - b;
+    },
+    div: function (a, b) {
+        // console.log(a / b);
+        return  a / b;
+    },
+    powerOf: function (a, b) {
+        // console.log(a ** b);
+        return  a ** b;
+    },
+};
+
+
+console.log(calculator.add(5, 1));
+console.log(calculator.minus(5, 1));
+console.log(calculator.div(5, 1));
+console.log(calculator.powerOf(5, 1));
+
+// 2.11 Returns & 2.12 Recap
+const plusResult = calculator.add(2, 3);
+const minusResult = calculator.minus(plusResult, 10);
+// const timesResult = calculator.times(10, minusResult);
+const divideResult = calculator.div(minusResult, plusResult);
+const powerResult = calculator.powerOf(divideResult, minusResult);
+
+const age = 23;
+function calculateKrAge(ageOfForeigner) {
+    ageOfForeigner + 2;
+    return "hello";
+}
+
+const krAge = calculateKrAge(age);
+
+console.log(krAge);
+
+// 2.13 Conditionals
+const age2 = parseInt(prompt("How old are you?"));
+
+console.log(isNaN());
+console.log(age2);
+//console.log(age2, parseInt("age2"));
+// console.log(typeof "15", typeof parseInt("15"));
+
+// true || true === true
+// false || true === true
+// true || false === true
+// false || fasle === false
+
+// true && true === true
+// false && true === false
+// true && false === false
+// false && false === false
+
+// 2.14 Conditionals par Two && 2.15 Conditionals part Three
+if (isNaN(age2) || age < 0) { // condition
+    // condition === true
+    console.log("Please write a  real positive number");
+} else if (age2 < 18) {
+    console.log("You are too young.");
+} else if (age2 >= 18 && age2 <= 50) {
+    console.log("You can drink");
+} else if (age2 > 50 && age2 <= 80) {
+    console.log("You should exercise");
+} else if (age2 > 80) {
+    // condition === false
+    console.log("You can do whatever you want.");
+} else if (age2  !== 100) {
+    console.log("wow you are wise");
+}
+
+// 2.16 Recap
+
+
